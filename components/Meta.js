2fe5f7d7000b8c0244d44fcd7e42f2364884c214
Maksim K <hi@karalevich.com>
2019-12-30 23:21:12 -0800

    add fav and og

--- components/Meta.js
+++ components/Meta.js
@@ -4,9 +4,18 @@ import Head from 'next/head'
 export const Meta = () => {
 	return (
 		<Head>
-		  <title>Freemia School—Family Education</title>
 		  <meta charSet='utf-8' />
 		  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+  	  <meta name="Freemia School · Family Education" />
+    	<meta name="description" content="{{ page.description }}" />
+			<meta name="keywords" content="education, unschooling, free-schooling, school" />
+		  
+		  <meta property="og:title"   content="Freemia School · Family Education" />
+    	<meta property="og:url"     content="https://freemia.school" />
+    	<meta property="og:image"   content="/og.jpg" />
+		  <link rel="icon" href="/fav.png" />
+		  <title>Freemia School—Family Education</title>
 		</Head>
 	)
 }
