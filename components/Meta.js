3e28c36ce3afe99ffbedd81f2c9e3acdf1668713
Maksim K <hi@karalevich.com>
2019-12-31 08:03:28 -0800

    add og properties

--- components/Meta.js
+++ components/Meta.js
@@ -11,9 +11,12 @@ export const Meta = () => {
     	<meta name="description" content="A personalised learning resource for self-directed learners." />
 			<meta name="keywords" content="education, unschooling, free-schooling, school" />
 		  
-		  <meta property="og:title"   content="Freemia School · Family Education" />
-    	<meta property="og:url"     content="https://freemia.school" />
-    	<meta property="og:image"   content="/og.jpg" />
+    	<meta property="og:image"					content="/og.jpg" />
+		  <meta property="og:title"					content="Freemia School · Family Education" />
+		  <meta property="og:description"		content="A personalised learning resource for self-directed learners."
+		   />
+		   <meta property="og:type"					content="website" />
+    	<meta property="og:url"						content="https://freemia.school" />
 		  <link rel="icon" href="/fav.png" />
 		  <title>Freemia School · Family Education</title>
 		</Head>
